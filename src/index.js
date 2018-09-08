f2e7e7469bcd7ee87659b28e2e2bf05629d1d5f1
Øystein Kjærnet <kjarnet@gmail.com>
2018-09-08 21:13:35 +0200

    - autorun - strip down interface - focus new lines - remove old lines

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
   terrainProvider: Cesium.createWorldTerrain({
     requestWaterMask: true
   }),
-  requestVertexNormals: true
+  requestVertexNormals: true,
+  animation: false,
+  infoBox: false,
+  sceneModePicker: false,
+  selectionIndicator: false,
+  timeline: false,
+  scene3DOnly: true,
+  shouldAnimate: true,
+  creditContainer: 'cesiumCreditsContainer'
+  //terrainExaggeration: 100.0 // Fun! :)
 })
 
 viewer.scene.globe.enableLighting = true
 
 function addLine(lat, long, height, id, startTime) {
-  console.log('Adding line!')
+  console.log(`Adding line ${id} at ${startTime}!`)
 
   const color = Cesium.Color.fromHsl((0.6 - (height * 0.5)), 1.0, 0.5)
 
@@ -21,30 +30,12 @@ function addLine(lat, long, height, id, startTime) {
   polyline.followSurface = new Cesium.ConstantProperty(false)
   polyline.positions = new Cesium.CallbackProperty(function(time, result) {
     const t = Cesium.JulianDate.secondsDifference(time, startTime);
-    const curHeight = t > 3 ? height : height * t / 3
+    const curHeight = t > 3 ? height : height * t / 3 // Grow from zero to height in 3 sec, then stop
+    console.log(`Setting height ${curHeight} for ${id} at ${time}`)
     const surfacePosition = Cesium.Cartesian3.fromDegrees(long, lat, 0)
-    const heightPosition = Cesium.Cartesian3.fromDegrees(long, lat, curHeight * 1000000)
+    const heightPosition = Cesium.Cartesian3.fromDegrees(long, lat, curHeight * 1e6)
     return [surfacePosition, heightPosition]
   }, false)
-
-  /*
-  t = (time - startTime)
-
-  t=3 <=> h=H
-
-  v = dh/dt
-
-  dh = v*dt
-
-  H = v * 3
-  v = H / 3
-
-  dh = (H/3)*dt
-
-  h = H*t/3
-
-
-  */
 
 
   const entity = new Cesium.Entity({
@@ -56,16 +47,23 @@ function addLine(lat, long, height, id, startTime) {
 
 //Add the entity to the collection.
   viewer.entities.add(entity)
+  viewer.flyTo(entity, {
+    duration: 2.0,
+    offset: new Cesium.HeadingPitchRange(0, -0.7, 20e6)
+  })
+  window.setTimeout(() => viewer.entities.remove(entity), 4000)
 
 }
 
 
 window.setTimeout(() => addLine(59.8239727, 10.4011683, 1, 1, Cesium.JulianDate.now()), 2000)
 
-window.setTimeout(() => addLine(59.8239727, 21.4011683, 2, 2, Cesium.JulianDate.now()), 4000)
+window.setTimeout(() => addLine(49.0, 21.0, 2, 2, Cesium.JulianDate.now()), 4000)
 
-window.setTimeout(() => addLine(59.8239727, 32.4011683, 3, 3, Cesium.JulianDate.now()), 6000)
+window.setTimeout(() => addLine(30.0, 42.0, 3, 3, Cesium.JulianDate.now()), 6000)
 
-window.setTimeout(() => addLine(59.8239727, 43.4011683, 4, 4, Cesium.JulianDate.now()), 8000)
+window.setTimeout(() => addLine(36.0, 33.0, 4, 4, Cesium.JulianDate.now()), 8000)
+
+window.setTimeout(() => addLine(-20.0, -63.0, 3, 5, Cesium.JulianDate.now()), 9000)
 
 
